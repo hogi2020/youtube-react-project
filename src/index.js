cfda443dcddc52b0707752d7ba6968679185394e
hogi2020 <junmyung2020@gmail.com>
2025-01-04 15:34:58 +0900

    react project start

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 import App from './App';
-import './assets/scss/style.scss'
+import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // React.StrictMode는 앱 내의 컴포넌트들을 엄격한 모드로 실행하도록 돕습니다.
-  // 앱을 개발할 때, 경고 및 잠재적인 문제를 식별하기 위한 도구로 사용됩니다.
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
