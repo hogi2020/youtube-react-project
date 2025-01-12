00c852392f546053a8348da5db25730005dfc0ea
hogi2020 <junmyung2020@gmail.com>
2025-01-13 00:34:25 +0900

    추천 영상 작업

--- src/components/contents/Today.jsx
+++ src/components/contents/Today.jsx
@@ -1,8 +1,32 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { todayText } from '../../data/today'
 
 const Today = () => {
     return (
-        <div>Today</div>
+        <section>
+            <div>
+                <div>
+                    <Link to={todayText[0].page}>
+                        <img src={todayText} alt={todayText[0].title} />
+                    </Link>
+                </div>
+                
+                <div>
+                    <span className='today__text'>today!</span>
+                    <h3 className='title'>
+                        <Link to={todayText[0].page}>{todayText[0].title}</Link>
+                    </h3>
+                    <p className='desc'>{todayText[0].desc}</p>
+                    <div className='info'>
+                        <span className='author'>
+                            <Link to={`/channel/${todayText[0].channelId}`}>{todayText[0].author}</Link>
+                        </span>
+                        <span className='date'>{todayText[0].date}</span>
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 
